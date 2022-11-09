5388609327f2f033ab772d7fae6456c3d742952d
bkleinb78 <y2bk728@aol.com>
2022-11-09 06:45:28 -0600

    added the paths in drf_jwt_backend + music urls.py files

--- backend/music/urls.py
+++ backend/music/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('', views.tracks_list),
-    path('<int:pk>/', views.track_detail),
+    path('', views.music_list),
+    path('<int:pk>/', views.music_detail),
 ]
